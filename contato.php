a57edcbb6635e3a9e926f66b38f4a9bf08d34988
Claudio Antunes <pesquisaforte@gmail.com>
2014-07-08 23:34:06 -0300

    Projeto Macarrone

---
+++ contato.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+      
+      <?php 
+        
+        require_once('menu.php');
+       ?>
+<!--        <div>           <?php  require_once($_GET["parametro"]);  ?>
+        </div>
+        -->
+       
+         <div>
+            <?php
+            echo "Todos os direitos reservados - ", date('Y');
+            
+            ?>
+        </div>
+    </body>
+</html>
---
+++ menu.php
@@ -0,0 +1,10 @@
+        <div>
+            <ul>
+                <li><a href="index.php">Home</li>
+                <li><a href="empresa.php">Empresa</li>
+                <li><a href="produtos.php">Produtos</li>
+                <li><a href="servicos.php">Serviços</li>
+                <li><a href="contato.php">Contato</li>
+                
+            </ul>
+        </div>
